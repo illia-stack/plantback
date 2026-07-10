656e4c45e0e913e2b6af37ed7d47a861203e5688
illia-stack <illiashapshalov38@gmail.com>
2026-07-10 20:45:29 +0200

    hit

--- api/send-contact.php
+++ api/send-contact.php
@@ -1,5 +1,6 @@
 <?php
-
+echo "HIT";
+exit;
 // ✅ CORS (ONLY what this endpoint needs)
 header("Access-Control-Allow-Origin: https://plantfront.onrender.com");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
