876f3b01bebee0b21f1b17f5c77470a83ab8df5f
illia-stack <illiashapshalov38@gmail.com>
2026-07-10 16:56:09 +0200

    contact backend fix

--- api/send-contact.php
+++ api/send-contact.php
@@ -1,7 +1,8 @@
 <?php
+require_once __DIR__ '/..includes/bootstrap.php';
 header("Access-Control-Allow-Origin: https://plantfront.onrender.com");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, X-CSRF-Token");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
@@ -19,9 +20,13 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     exit;
 }
 
+
 // -------------------------
 // DATA
 // -------------------------
+
+validate_csrf();
+
 $data = json_decode(file_get_contents("php://input"), true);
 
 $name = trim($data['name'] ?? '');
